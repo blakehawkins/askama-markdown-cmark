0e5e0f04635c3c7624d7fda680ed15954d35e2b9
blakehawkins <blake.hawkins.11@gmail.com>
2025-10-25 10:26:52 +0100

    publish 0.2.0
    bumps askama

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@ pub mod filters {
     use pulldown_cmark;
 
     /// Renders text to HTML from Markdown.
-    pub fn markdown_cmark(s: &dyn fmt::Display) -> ::askama::Result<String> {
+    pub fn markdown_cmark<T: fmt::Display>(
+        s: T,
+        _: &dyn askama::Values,
+    ) -> ::askama::Result<String> {
         let text = &s.to_string();
         let parser = pulldown_cmark::Parser::new_ext(text, pulldown_cmark::Options::all());
 
@@ -17,13 +20,21 @@ pub mod filters {
 
 #[cfg(test)]
 mod tests {
-    use super::filters::*;
+    use super::filters;
+    use askama::Template;
+
+    #[derive(Template)]
+    #[template(source = "{{ s | markdown_cmark }}", ext = "md")]
+    struct ExampleCmark<'a> {
+        s: &'a str,
+    }
 
     #[test]
     fn markdown() {
-        assert_eq!(
-            markdown_cmark(&"*Simple* ipsum").unwrap(),
-            "<p><em>Simple</em> ipsum</p>\n"
-        );
+        let t = ExampleCmark {
+            s: "*Simple* ipsum",
+        };
+
+        assert_eq!(t.render().unwrap(), "<p><em>Simple</em> ipsum</p>\n");
     }
 }
